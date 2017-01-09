83879cc3a47b21222242b0a5f7ad7cfd280cb75c
Chā <chavis.clark@gmail.com>
2017-01-08 22:49:04 -0500

    add new SayHi methods

--- index.js
+++ index.js
@@ -5,11 +5,38 @@
     return new SayHi.init(firstName, lastName, language);
   }
 
-  SayHi.prototype = {};
+  var supportedLangs = ['en', 'es'];
+
+  //Set messages for English and Spanish
+  //These are only exposed to this enclosed object
+  var greetings = {
+    en: 'Hello',
+    es: 'Hola'
+  };
+
+  var formalGreetings = {
+    en: 'Greetings',
+    es: 'Saludos'
+  };
+
+  var logMessages = {
+    en: 'Logged in',
+    es: 'Inició sesión'
+  };
+
+  SayHi.prototype = {
+    fullName: function() {
+      return this.firstName + ' ' + this.lastName;
+    },
+
+    validate: function() {
+      
+    }
+  };
 
   SayHi.init = function(firstName, lastName, language) {
+      //Build properties for new insatnces of SayHi
     var self = this;
-
     self.firstName = firstName || '';
     self.lastName = lastName || '';
     self.language = language || 'en';
